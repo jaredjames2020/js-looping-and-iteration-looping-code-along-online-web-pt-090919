86d2f980deb9074a44b5448d6417799ffd97577d
Jared S <jaredjames2020@gmail.com>
2020-03-26 21:54:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solutions in this file
 
 function writeCards(name, event) {
-  const giftCards = []
-  for (let counter = 0; counter < name.length; counter++){
+  const giftCards = [];
+  for (let counter = 0; counter < name.length; counter++) {
       giftCards.push(`Thank you, ${name[counter]}, for the wonderful ${event} gift!`);
     }
   return giftCards;
@@ -11,8 +11,8 @@ function writeCards(name, event) {
 function countDown (integer) {
     let i = integer;
     while (integer > 0) {
-        console.log( integer )
+        console.log(integer)
         integer = integer - 1
     }
-    console.log( integer );
+    console.log(integer);
 }
