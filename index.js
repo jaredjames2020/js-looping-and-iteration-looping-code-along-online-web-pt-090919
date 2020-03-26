d27fecd0cbdaa0a3e9c34ddc877ed4c16a26f1a4
Jared S <jaredjames2020@gmail.com>
2020-03-26 21:50:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // Code your solutions in this file
 
 function writeCards(name, event) {
-  const giftCards = [];
+  const giftCards = []
+  
   for(let counter = 0; counter < writeCards[0].length; counter++)
-  {
-    console.log(`Thank you, ${name[counter]}, for the wonderful ${event} gift!`);
-  }
-  return writeCards();
+    {
+      console.log(`Thank you, ${name[counter]}, for the wonderful ${event} gift!`);
+    }
+  return giftCards();
 }
 
 writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' );
