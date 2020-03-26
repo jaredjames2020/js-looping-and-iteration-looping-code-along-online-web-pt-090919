1cd4f4474d89ec03c43659932aaaae61d61587df
Jared S <jaredjames2020@gmail.com>
2020-03-26 21:32:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+
+function writeCards(name, event) {
+  for(let counter = 0; counter <= writeCards.length; counter++)
+  {
+    console.log(`Thank you, ${name[counter]}, for the wonderful ${event} gift!`);
+  }
+  return writeCards;
+}
+
+writeCards( [ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
+
